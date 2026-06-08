2b02b04b7330ce6a4de7b074fc0332d445199b65
joegamal <yusufmagamalth@gmail.com>
2026-06-09 02:48:04 +0300

    the actual start

--- NetSpeed/Program.cs
+++ NetSpeed/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ///
+        /// how to measure the speed of the network
+        /// we need to monitor the input and output of the device 
+        /// and then see the capacity per second
+        
+
+        //step one monitor the input to out device
+        // this step will be divided into a components 
+        //the first component is how many processes are there running 
     }
 }
